6789d509dc8ab17fc3a74f0f5422754bf4dd01e2
Sudhakar Patnaik <sudhakarpartnaik.mca2009@gmail.com>
2020-12-01 21:15:33 +0530

    my second change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to INDIAN TRAIN TRAVEL
+          Welcome to ITT - INDIAN TRAIN TRAVEL
         </p>
         <a
           className="App-link"
